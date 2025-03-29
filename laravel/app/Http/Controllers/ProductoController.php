7e3b9f631d1fbb882f7cf154d9f666c10376dd4d
Joel Medina <95372286+jjmmcode@users.noreply.github.com>
2025-03-29 02:23:45 -0300

    feat: implementar CRUD completo para productos
    Creé el modelo Producto, la migración y el controlador.
    
    Se implementaron las rutas y métodos GET, POST, PUT y DELETE usando Postman para pruebas.
    
    Todos los métodos funcionan correctamente y responden con JSON.

--- laravel/app/Http/Controllers/ProductoController.php
+++ laravel/app/Http/Controllers/ProductoController.php
@@ -3,10 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Producto;
 
 class ProductoController extends Controller
 {
     public function obtener_productos() {
         return 'Llegamos a la función';
+    }
+    public function obtener_producto($id) {
+        return "Producto con ID: " . $id;
+    }
+
+    public function store(Request $request) {
+        $producto = Producto::create([
+            'nombre' => $request->input('nombre'),
+            'precio' => $request->input('precio'),
+        ]);
+
+        return response()->json($producto, 201);
+    }
+
+    public function destroy($id)
+    {
+        $producto = Producto::find($id);
+
+        if (!$producto) {
+            return response()->json(['mensaje' => 'Producto no encontrado'], 404);
+        }
+
+        $producto->delete();
+
+        return response()->json(['mensaje' => 'Producto eliminado'], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $producto = Producto::find($id);
+
+        if(!$producto) {
+            return response()->json(['mensaje' => 'Producto no encontrado'], 404);
+        }
+
+        $producto->update([
+            'nombre' => $request ->input('nombre'),
+            'precio' => $request ->input('precio'),
+        ]);
+
+        return response()->json($producto, 200);
     }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
 
 Route::get('/productos', [ProductoController::class, 'obtener_productos']);
+Route::get('/productos/{id}', [ProductoController::class, 'obtener_producto']);
+Route::post('/productos', [ProductoController::class, 'store']);
+Route::delete('/productos/{id}', [ProductoController::class, 'destroy'])->whereNumber('id');
+Route::put('/productos/{id}', [ProductoController::class, 'update'])->whereNumber('id');
