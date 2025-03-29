d68221919280e2fdb7e639972b263144210be80c
Joel Medina <95372286+jjmmcode@users.noreply.github.com>
2025-03-28 23:21:39 -0400

    feat: Creación de rutas
    Creé la ruta de obtener_productos que llega al controlador productos que también llega a la función productos

---
+++ laravel/app/Http/Controllers/ProductoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    public function obtener_productos() {
+        return 'Llegamos a la función';
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/productos', [ProductoController::class, 'obtener_productos']);
