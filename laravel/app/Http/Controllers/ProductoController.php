fbaf3d30a22edff1805558f288a1cb9730cb1b67
Ignacio Veliz <101842633+markawaii@users.noreply.github.com>
2025-05-24 14:00:20 -0400

    Update ProductoController.php

--- laravel/app/Http/Controllers/ProductoController.php
+++ laravel/app/Http/Controllers/ProductoController.php
@@ -9,21 +9,29 @@ use App\Models\TipoProducto;
 
 class ProductoController extends Controller
 {
-    public function obtener_productos() {
+    public function index()
+    {
+        return view('components.productos.index');
+    }
+
+    public function obtener_productos()
+    {
         $producto = Producto::with('marca')->get();
 
         return response()->json([
             'status' => 'success',
             'message' => 'Lista de productos obtenida correctamente',
-            'data' => $producto]);
+            'data' => $producto
+        ]);
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         // dd($request->all());
 
         $marca = Marca::where('id', $request->input('brand_id'))->where('active', true)->first();
 
-        if(!$marca) {
+        if (!$marca) {
             return response()->json(['status' => 'error', 'message' => 'La marca no existe o está inactiva'], 404);
         }
 
@@ -31,21 +39,22 @@ class ProductoController extends Controller
             'name' => $request->input('name'),
             'price' => $request->input('price'),
             'description' => $request->input('description'),
-            'active' => $request->input('active') ? true:false,
+            'active' => $request->input('active') ? true : false,
             'stock' => $request->input('stock'),
             'sku' => $request->input('sku'),
             'brand_id' => $marca->id,
-            'type_id' =>$request->input('type_id'),
+            'type_id' => $request->input('type_id'),
         ]);
 
         $respuesta = [
             'producto_id' => $producto->id,
         ];
 
-        return response()->json(['status' => 'success', 'message' => 'Producto creado correctamente' , 'data' => $respuesta]);
+        return response()->json(['status' => 'success', 'message' => 'Producto creado correctamente', 'data' => $respuesta]);
     }
 
-    public function destroy(Request $request) {
+    public function destroy(Request $request)
+    {
         $id = $request->input('id');
         $producto = Producto::find($id);
 
@@ -55,35 +64,28 @@ class ProductoController extends Controller
 
         $producto->delete();
 
-        return response()->json(['message'=> 'Producto eliminado']);
+        return response()->json(['message' => 'Producto eliminado']);
     }
 
     public function update(Request $request, $id)
-        {
-            $producto = Producto::find($id);
+    {
+        $producto = Producto::find($id);
 
-            if (!$producto) {
-                return response()->json(['message' => 'Producto no encontrado'], 404);
-            }
-
-            $producto->update([
-                'name' => $request->input('name'),
-                'price' => $request->input('price'),
-                'description' => $request->input('description'),
-                'stock' => $request->input('stock'),
-                'sku' => $request->input('sku'),
-                'active' => $request->input('active') ? true : false,
-                'brand_id' => $request->input('brand_id'),
-                'type_id' => $request->input('type_id'),
-            ]);
-
-            return response()->json($producto);
+        if (!$producto) {
+            return response()->json(['message' => 'Producto no encontrado'], 404);
         }
 
-    public function index() {
-        $productos = Producto::all();
-        $marcas = Marca::all();
-        $tipos = TipoProducto::all();
-        return view('components.productos.index', compact('productos', 'marcas', 'tipos'));
+        $producto->update([
+            'name' => $request->input('name'),
+            'price' => $request->input('price'),
+            'description' => $request->input('description'),
+            'stock' => $request->input('stock'),
+            'sku' => $request->input('sku'),
+            'active' => $request->input('active') ? true : false,
+            'brand_id' => $request->input('brand_id'),
+            'type_id' => $request->input('type_id'),
+        ]);
+
+        return response()->json($producto);
     }
 }
